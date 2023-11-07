bc526c419f2a646d96963fd3770a5aaf51901e78
kevst-dev <kevst-dev@protonmail.com>
2023-11-07 16:09:16 -0500

    feat: respond to a HTTP request with a 200 OK response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let response = format!(r"HTTP/1.1 200 OK\r\n\r\n");
+
+                stream.write_all(response.as_bytes()).unwrap();
             },
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(_) => println!("OOF"),
         }
     }
 }
