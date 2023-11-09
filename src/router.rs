107b83a8eb7e1ff25b5e84a3a0e97e991db551e7
kevst-dev <kevst-dev@protonmail.com>
2023-11-08 20:11:21 -0500

    fix: bug to route '/'

--- src/router.rs
+++ src/router.rs
@@ -31,10 +31,28 @@ impl Router {
 
                             let _ = response.send_response(stream);
                         }
-                         // Else, invoke static page handler
-                        _ => {
+                        // if '/'
+                        "" => {
                             let status_code = "200";
                             let body = "Todo en orden pero no conozco la ruta";
+                            let mut headers = HashMap::new();
+                            headers.insert(
+                                "Content-type".to_string(), "text/plain".to_string()
+                            );
+
+                            let response = HttpResponse::new(
+                                status_code,
+                                Some(headers),
+                                Some(body.to_string()),
+                            );
+
+                            let _ = response.send_response(stream);
+                        },
+                        _ => {
+                            let status_code = "404";
+                            let body = format!(
+                                "HTTP {:?} method not supported", request.method
+                            );
                             let mut headers = HashMap::new();
                             headers.insert(
                                 "Content-type".to_string(), "text/plain".to_string()
